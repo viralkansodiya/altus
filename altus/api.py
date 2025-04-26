3860abb853a423047e0a3b96ea99a6e42689948b
viral <viral@fosserp.com>
2025-04-26 12:08:16 +0000

    ch

--- altus/api.py
+++ altus/api.py
@@ -3,7 +3,6 @@ import frappe
 @frappe.whitelist()
 def check_search_bar_per(user):
     roles = frappe.get_roles(user)
-    from frappe.core.doctype.role.role import desk_properties
     for row in roles:
         role_doc = frappe.db.get_value("Role", row, "search_bar")
         if role_doc:
